23fc33611abba7ac4de932d415fe6b3676cbb4a4
darking <darkling@hotmail.com>
2015-10-28 11:40:45 +0300

    re added model and connection manager

---
+++ Tremble-iOS/Tremble-iOS/ActiveSession.swift
@@ -0,0 +1,23 @@
+//
+//  ActiveSession.swift
+//  Tremble
+//
+//  Created by Abduallah Al Mashmoum on 10/27/15.
+//  Copyright © 2015 Techzone. All rights reserved.
+//
+
+import Foundation
+
+class ActiveSession {
+    
+    var idSession:Int
+    
+    init (idSession:Int) {
+        
+        self.idSession = idSession
+        
+    }
+    
+    
+    
+}
